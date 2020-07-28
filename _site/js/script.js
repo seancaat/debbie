a578944c752b5f51259b0fd9738372c997a8104d
seancaat <seancaat@gmail.com>
2020-07-28 19:49:19 -0400

    padding tweaks

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,5 +1,5 @@
-var widths = ["mw5", "mw6", "mw7"];
 // different width set on mobile
+var widths = (window.innerWidth > 500) ? ["mw5", "mw6", "mw7"] : ["mw5", "mw6"];
 var offsets = ["ml0", "ml1", "ml2", "ml3", "ml4", "ml5", "ml6", "ml7"];
 var floats = ["fl", "fr"];
 var cards = document.querySelectorAll(".gallery-card ");
@@ -14,6 +14,8 @@ for(var i = 0; i < cards.length; i++){
     cards[i].classList.add(randof(widths));
     // cards[i].classList.add(randof(offsets));
 }
+
+
 
 function randof(array) {
     return(array[Math.floor(Math.random() * array.length)]);
