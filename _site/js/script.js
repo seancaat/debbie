de5c2aaadecfba04c012cd3774f961e3d8328188
seancaat <seancaat@gmail.com>
2020-07-28 19:25:04 -0400

    nav fixes, rid of clearfix

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,10 +1,16 @@
 var widths = ["mw5", "mw6", "mw7"];
 var offsets = ["ml0", "ml1", "ml2", "ml3", "ml4", "ml5", "ml6", "ml7"];
+var floats = ["fl", "fr"]
+
+
 var cards = document.querySelectorAll(".gallery-card ");
 
 for(var i = 0; i < cards.length; i++){
+    // right or left 
+    cards[i].classList.add(randof(floats));    
+
     cards[i].classList.add(randof(widths));
-    cards[i].classList.add(randof(offsets));
+    // cards[i].classList.add(randof(offsets));
 }
 
 function randof(array) {
