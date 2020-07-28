eefe3fcd9a356b931295566d361fbd0c7cf675fc
seancaat <seancaat@gmail.com>
2020-07-28 18:19:20 -0400

    new image behavior, captions, generative sizes, generative offsets.

---
+++ _site/js/script.js
@@ -0,0 +1,12 @@
+var widths = ["mw5", "mw6", "mw7"];
+var offsets = ["ml0", "ml1", "ml2", "ml3", "ml4", "ml5", "ml6", "ml7"];
+var cards = document.querySelectorAll(".gallery-card ");
+
+for(var i = 0; i < cards.length; i++){
+    cards[i].classList.add(randof(widths));
+    cards[i].classList.add(randof(offsets));
+}
+
+function randof(array) {
+    return(array[Math.floor(Math.random() * array.length)]);
+}
