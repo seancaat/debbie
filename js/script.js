8327e7f540e8930d131bef9d594ae4721fbab197
seancaat <seancaat@gmail.com>
2020-07-31 15:38:28 -0400

    clearfix for container height and bottom padding

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 // different width set on mobile
-var widths = (window.innerWidth > 500) ? ["mw5", "mw6", "mw7"] : ["mw5", "mw6"];
-var offsets = ["ml0", "ml1", "ml2", "ml3", "ml4", "ml5", "ml6", "ml7"];
-var floats = ["fl", "fr", "middle"];
+var widths = (window.innerWidth > 500) ? ["mw5", "mw6", "mw7"] : ["mw4", "mw4", "mw5"];
+var floats = (window.innerWidth > 500) ? ["fl", "fr"] : ["fl", "fr", "middle", "middle"];
 var cards = document.querySelectorAll(".gallery-card ");
 
 document.querySelector("main").style.opacity = "1.0";
 console.log(document.querySelector("main"));
 
 for(var i = 0; i < cards.length; i++){
-    // right or left 
-    cards[i].classList.add(randof(floats));  
-
+    cards[i].classList.add(randof(floats));
     cards[i].classList.add(randof(widths));
-    // cards[i].classList.add(randof(offsets));
 }
 
 
